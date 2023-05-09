d9a48816078f9a0e083defd8de7bdbae52641909
lghernandezp1 <88736572+lghernandezp1@users.noreply.github.com>
2023-05-08 19:51:55 -0500

    Add files via upload

---
+++ Serie.js
@@ -0,0 +1,21 @@
+var Serie = /** @class */ (function () {
+    
+    function Serie(number, title, channel, seasons, description, link, image) {
+        
+        this.number = number;
+        
+        this.title = title;
+        
+        this.channel = channel;
+        
+        this.seasons = seasons;
+        
+        this.description = description;
+        
+        this.link = link;
+        
+        this.image = image;
+    }
+    return Serie;
+}());
+export { Serie };
